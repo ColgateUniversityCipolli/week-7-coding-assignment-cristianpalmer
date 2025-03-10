8a4a6a1059972ef21dd2b718193317190daed924
cristianpalmer <cpalmer@colgate.edu>
2025-03-10 13:07:09 -0400

    Draft, ask about putting restrictions

--- HW7WorkingR.R
+++ HW7WorkingR.R
@@ -6,19 +6,24 @@
 
 
 pois.prob <- function(x, lambda, type=""){
+#Calculates P(X = x) 
   if (type == "=") {
-    return(dpois(x, lambda))
-} else if (type == "!=") {
-    return(1 - dpois(x, lambda))
-} else if (type == "<") {
-    return(ppois(x - 1, lambda))
-} else if (type == "<=") {
-    return(ppois(x, lambda))
-} else if (type == ">") {
-    return(1 - ppois(x, lambda))
-} else if (type == ">=") {
-    return(1 - ppois(x - 1, lambda))
-}
+    return(dpois(x, lambda)) } 
+#Calculates P(X ̸= x)   
+  else if (type == "!=") {
+    return(1 - dpois(x, lambda)) }
+#Calculates P(X < x) 
+  else if (type == "<") {
+    return(ppois(x - 1, lambda)) }
+#Calculates  P(X ≤ x) 
+  else if (type == "<=") {
+    return(ppois(x, lambda)) }
+#Calculates P(X > x) 
+  else if (type == ">") {
+    return(1 - ppois(x, lambda)) }
+#Calculates  P(X ≥ x)   
+  else if (type == ">=") {
+    return(1 - ppois(x - 1, lambda)) }
 }
 ########################################################################
 
@@ -30,17 +35,22 @@ pois.prob <- function(x, lambda, type=""){
 #user to specify the shape parameters α and β.
 
 beta.prob <- function(x, alpha, beta, type=""){
+#Calculates P(X = x)
   if (type == "=") {
-    return(dbeta(x, alpha, beta))
-  } else if (type == "!=") {
-    return(1 - dbeta(x, alpha, beta))
-  } else if (type == "<") {
-    return(pbeta(x - 1, alpha, beta))
-  } else if (type == "<=") {
-    return(pbeta(x, alpha, beta))
-  } else if (type == ">") {
-    return(1 - pbeta(x, alpha, beta))
-  } else if (type == ">=") {
-    return(1 - pbeta(x - 1, alpha, beta))
-  }
+    return(dbeta(x, alpha, beta)) }
+#Calculates P(X ̸= x)
+  else if (type == "!=") {
+    return(1 - dbeta(x, alpha, beta)) }
+#Calculates  P(X < x)
+  else if (type == "<") {
+    return(pbeta(x - 1, alpha, beta)) }
+#Calculates , P(X ≤ x)
+  else if (type == "<=") {
+    return(pbeta(x, alpha, beta)) }
+#Calculates  P(X > x)
+  else if (type == ">") {
+    return(1 - pbeta(x, alpha, beta)) }
+#Calculates P(X ≥ x)
+  else if (type == ">=") {
+    return(1 - pbeta(x - 1, alpha, beta)) }
 }
